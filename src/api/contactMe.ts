2cad0fbbb8c2cdbb6d9f87a3112b7cb04eb4c90b
Jannik Will <jw19980309@gmail.com>
2022-10-12 12:39:05 +0200

    update api

--- src/api/contactMe.ts
+++ src/api/contactMe.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { ContactMeData } from "../types/contactMeData";
 import { createTransport, createTestAccount } from "nodemailer";
+import Mailgen from "mailgen";
 
 let testAccount = createTestAccount();
 
@@ -17,13 +18,38 @@ let transporter = createTransport({
 const contactMeHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const value = req.body as ContactMeData;
+    const mailGenerator = new Mailgen({
+      theme: "default",
+      product: {
+        // Appears in header & footer of e-mails
+        name: "Jane Will",
+        link: "https://personal-page-ivory.vercel.app/",
+        copyright: "Copyright © 2022 Jane Will. All rights reserved.",
+      },
+    });
+    const email: Mailgen.Content = {
+      body: {
+        name: "Jane",
+        intro: "Someone contacted you via your contact form",
+        action: {
+          instructions: "To answer, please click here:",
+          button: {
+            color: "#347eb6", // Optional action button color
+            text: "Answer",
+            link: `mailto:${value.email}`,
+          },
+        },
+      },
+    };
+    const emailBody = mailGenerator.generate(email);
+    const emailText = mailGenerator.generatePlaintext(email);
 
-    let info = await transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: "bar@example.com, baz@example.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
+    await transporter.sendMail({
+      from: `"${value.name}" <${value.email}>`, // sender address
+      to: `${process.env["EMAIL_USERNAME"]}`, // list of receivers
+      subject: "You got an contact request", // Subject line
+      text: emailText, // plain text body
+      html: emailBody, // html body
     });
     res.status(200).end();
   } catch (err) {
