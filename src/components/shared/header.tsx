0433e55d88f8f2f79ea5a8a1bb69657b3a78e97c
Jannik Will <jw19980309@gmail.com>
2022-10-12 12:17:17 +0200

    Update header

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -22,23 +22,21 @@ export const Header: React.FC<PropsWithChildren<Props>> = ({
 }) => {
   return (
     <header className={`w-screen min-h-screen relative ${className} `}>
-      <div className="absolute top-0 left-0 w-full min-h-screen">
-        <div className="relative w-[100vw_+_env(safe-area-inset-left)] min-h-screen">
-          <Image
-            src={src}
-            alt="Man with Rainbow Flag"
-            layout="fill"
-            className="object-cover"
-            priority
-          ></Image>
-          <div className="absolute bg-[#0c111663] top-0 left-0 w-full h-full z-10" />
-          <div className="relative z-20 px-ios-4 sm:px-ios-2 lg:px-ios-0 py-16 w-screen min-h-screen flex items-center justify-center">
-            {multipleChildren ? (
-              children
-            ) : (
-              <div className="max-w-7xl">{children}</div>
-            )}
-          </div>
+      <div className="relative w-[100vw_+_env(safe-area-inset-left)] min-h-screen">
+        <Image
+          src={src}
+          alt="Man with Rainbow Flag"
+          layout="fill"
+          className="object-cover"
+          priority
+        ></Image>
+        <div className="absolute bg-[#0c111663] top-0 left-0 w-full h-full z-10" />
+        <div className="relative z-20 px-ios-4 sm:px-ios-2 lg:px-ios-0 py-16 w-screen min-h-screen flex items-center justify-center">
+          {multipleChildren ? (
+            children
+          ) : (
+            <div className="max-w-7xl">{children}</div>
+          )}
         </div>
       </div>
     </header>
