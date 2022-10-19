9627d6bffe6a2659ae934e9e6e8c1b1430f40381
Jannik Will <jw19980309@gmail.com>
2022-10-19 17:15:24 +0200

    Update copyright

--- apps/portfolio/src/components/shared/footer.tsx
+++ apps/portfolio/src/components/shared/footer.tsx
@@ -84,17 +84,7 @@ export const Footer: React.FC<Props> = () => {
       <div className="bg-gray-100 dark:bg-gray-700 pb-ios-4">
         <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
           <p className="text-gray-700 dark:text-gray-100 text-sm text-center sm:text-left">
-            © 2022 Jane Will —
-            <Link
-              external
-              noUnderline
-              href="https://twitter.com/knyttneve"
-              rel="noopener noreferrer"
-              className="text-gray-600 dark:text-gray-200 ml-1"
-              target="_blank"
-            >
-              @sustainjane98
-            </Link>
+            © 2022 Jane Will
           </p>
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
             <Link
