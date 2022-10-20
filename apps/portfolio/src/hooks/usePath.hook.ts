7c969af542fd86ff33f4e520dfa102373c6366d5
Jannik Will <jw19980309@gmail.com>
2022-10-20 23:30:01 +0200

    Fix multilanguage

--- apps/portfolio/src/hooks/usePath.hook.ts
+++ apps/portfolio/src/hooks/usePath.hook.ts
@@ -1,10 +1,17 @@
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export const usePath = (paths: string[]) => {
   const [isPath, setIsPath] = useState(false);
 
+  const { locale } = useRouter();
+
   useEffect(() => {
-    const currentUrl = window.location.pathname + window.location.hash;
+    let currentUrl = window.location.pathname + window.location.hash;
+
+    if (locale != "en") {
+      currentUrl = currentUrl.replace("/" + locale ?? "", "");
+    }
 
     for (const path of paths) {
       if (currentUrl === path) {
