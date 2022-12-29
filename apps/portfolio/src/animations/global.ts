68b39c1164e5284135b483f6231b2f28356a076e
Jannik Will <jw19980309@gmail.com>
2022-12-29 23:32:04 +0100

    Remove Show Animation

--- apps/portfolio/src/animations/global.ts
+++
@@ -1,7 +0,0 @@
-import { Variants } from "framer-motion";
-
-export const globalAnimationVariants: Variants = {
-  hidden: { opacity: 0 },
-  enter: { opacity: 1 },
-  exit: { opacity: 0 },
-};
--- apps/portfolio/src/templates/skills.template.tsx
+++ apps/portfolio/src/templates/skills.template.tsx
@@ -8,8 +8,7 @@ import { HeadlineBody } from "../components/shared/headline-body";
 import { ScrollDownIndicator } from "../components/shared/scroll-down-indicator";
 import { Skills, Props as SkillsProps } from "../components/shared/skills";
 import { Button, Props as ButtonProps } from "../components/shared/button";
-import { AnimatePresence, motion } from "framer-motion";
-import { globalAnimationVariants } from "../animations/global";
+import { motion } from "framer-motion";
 import { ProfilePicture } from "../components/shared/profilePicture";
 import { headlineShowUpAnimation } from "../animations/headlines";
 import { DataTestIds } from "@portfolio/shared-testing";
@@ -39,64 +38,56 @@ export const SkillsTemplate: React.FC<Props> = ({
   githubProfileUrl,
 }) => {
   return (
-    <AnimatePresence>
-      <motion.div
-        variants={globalAnimationVariants}
-        initial="hidden" // Set the initial state to variants.hidden
-        animate="enter" // Animated state to variants.enter
-        exit="exit" // Exit state (used later) to variants.exit
-        transition={{ type: "linear" }} // Set the transition to linear
+    <div>
+      <Header
+        src={src}
+        className={className}
+        indicator={skills && <ScrollDownIndicator className="z-10" />}
+        source={source}
       >
-        <Header
-          src={src}
-          className={className}
-          indicator={skills && <ScrollDownIndicator className="z-10" />}
-          source={source}
-        >
-          {githubProfileUrl && (
-            <ProfilePicture
-              alt="Github Profile Picture"
-              src={githubProfileUrl}
-              layout="fill"
-            />
-          )}
-          <MotionHeadline
-            {...headline}
+        {githubProfileUrl && (
+          <ProfilePicture
+            alt="Github Profile Picture"
+            src={githubProfileUrl}
+            layout="fill"
+          />
+        )}
+        <MotionHeadline
+          {...headline}
+          variants={headlineShowUpAnimation}
+          initial="hidden"
+          animate="enter"
+        />
+        {body && (
+          <MotionBody
             variants={headlineShowUpAnimation}
             initial="hidden"
             animate="enter"
-          />
-          {body && (
-            <MotionBody
-              variants={headlineShowUpAnimation}
-              initial="hidden"
-              animate="enter"
-            >
-              {body}
-            </MotionBody>
-          )}
-          {buttons && (
-            <div className="mt-6 inline-flex">
-              {buttons.map((button, index) => (
-                <MotionButton
-                  variants={headlineShowUpAnimation}
-                  initial="hidden"
-                  animate="enter"
-                  key={index}
-                  {...(button as any)}
-                />
-              ))}
-            </div>
-          )}
-        </Header>
-        <main data-testid={DataTestIds.SKILL_BODY_CONTAINER}>
-          {skills && (
-            <div className="max-w-7xl mx-auto">
-              <Skills {...skills} />
-            </div>
-          )}
-        </main>
-      </motion.div>
-    </AnimatePresence>
+          >
+            {body}
+          </MotionBody>
+        )}
+        {buttons && (
+          <div className="mt-6 inline-flex">
+            {buttons.map((button, index) => (
+              <MotionButton
+                variants={headlineShowUpAnimation}
+                initial="hidden"
+                animate="enter"
+                key={index}
+                {...(button as any)}
+              />
+            ))}
+          </div>
+        )}
+      </Header>
+      <main data-testid={DataTestIds.SKILL_BODY_CONTAINER}>
+        {skills && (
+          <div className="max-w-7xl mx-auto">
+            <Skills {...skills} />
+          </div>
+        )}
+      </main>
+    </div>
   );
 };
